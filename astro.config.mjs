3cbe382f00112bb838ac14c9097ededd5b264621
Kadal Hitaam <naufalalex01@gmail.com>
2026-09-16 20:18:50 +0700

    Added cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -13,4 +15,8 @@ export default defineConfig({
 
   integrations: [react()],
 
+  output: 'server', // atau 'hybrid'
+  adapter: cloudflare(),
 });
+
+
